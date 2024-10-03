9f5d44c030465d3e14afd5739e8f4caa8da9135b
shouvick bachhar <bachharshouvickp2@gmail.com>
2024-10-03 23:02:11 +0530

    3rd day

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const { connection } = require("./config/db");
-const { userRoute } = require("./routes/user.route");
-const cors = require("cors");
+const express = require("express"); 
+const { connection } = require("./config/db"); 
+const { userRoute } = require("./routes/user.route"); 
+const cors = require("cors"); 
 
 const app = express();
 
